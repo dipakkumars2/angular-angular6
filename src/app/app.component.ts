28fc677f94368a097e779a2674537dc8208b5faf
dipakkumars2 <singh.dipakkumar.136@gmail.com>
2019-09-07 12:04:26 +0530

    2 files modified, 4 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular6 Tutorial';
+  name = 'Angular8 Tutorial';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ListEmployeeComponent } from './employee/list-employee/list-employee.component';
+import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ListEmployeeComponent, CreateEmployeeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
